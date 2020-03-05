8b62fd8dbb2eaa52ec51c29d2ae5256a679a583a
BandersGit <andy.i.flaskan@gmail.com>
2020-03-05 11:10:17 +0100

    Player can now choose horse

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
-            
+            string[] horses = { "1. A", "2. B", "3. C", "4. D" };
+            int money = 0;
+
+            Console.WriteLine("Welcome to the horse races!");
+
+            Console.WriteLine("I see you have " + money + " dollars");
+
+            Console.WriteLine("Please choose a horse to bet on");
+
+            for (int i = 0; i < horses.Length; i++)
+            {
+                Console.WriteLine(horses[i]);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Write the number of the horse you want to bet on!");
+
+
+            int horse = 
             //Bestäm användarens pengar
 
             //Kom in till hästbanan
@@ -26,7 +44,7 @@ namespace Slutprojekt
             //GO AGAIN?
 
 
-
+            Console.ReadLine();
 
         }
     }
