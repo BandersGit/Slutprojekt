65f5e57565c30d044688488c42ad6b1c068032cb
BandersGit <andy.i.flaskan@gmail.com>
2020-03-05 10:31:04 +0100

    Added a repository

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slutprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Bestäm användarens pengar
+
+            //Kom in till hästbanan
+
+            //Betta på en häst
+
+            //Hästarna springer
+
+            //Få in resultatet, jämför med hästan man bettade på
+
+            //Bestäm om användaren tjänar eller förlorar pengar
+
+            //GO AGAIN?
+
+
+
+
+        }
+    }
+}
