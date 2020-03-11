463ede3cef0d43af69a734b9b744fa806b5637c6
BandersGit <andy.i.flaskan@gmail.com>
2020-03-11 11:07:16 +0100

    Användaren kan nu välja häst och sätta pengar på den, med säkerhetsåtgärder ifall de inte svarar med ett giltigt svar

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Slutprojekt
 {
@@ -10,16 +11,16 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
-            string[] horses = { "1. A", "2. B", "3. C", "4. D" };
-            int money = 0;
+            string[] horses = { "Nope", "1. Roach", "2. Epona", "3. Agro", "4. Rapidash" };
+            int money = 1000;
 
             Console.WriteLine("Welcome to the horse races!");
 
             Console.WriteLine("I see you have " + money + " dollars");
 
-            Console.WriteLine("Please choose a horse to bet on");
+            Console.WriteLine("Here are todays horses!");
 
-            for (int i = 0; i < horses.Length; i++)
+            for (int i = 1; i < horses.Length; i++)
             {
                 Console.WriteLine(horses[i]);
                 Console.WriteLine("");
@@ -27,8 +28,61 @@ namespace Slutprojekt
 
             Console.WriteLine("Write the number of the horse you want to bet on!");
 
+            string horse = Console.ReadLine();
 
-            int horse = 
+            int.TryParse(horse, out int horseInt);
+
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            while (horseInt == 0 || horseInt > 4)
+            {
+                Console.WriteLine("That is not a valid number, sir");
+                Console.WriteLine("Please choose again");
+
+                for (int i = 1; i < horses.Length; i++)
+                {
+                    Console.WriteLine(horses[i]);
+                    Console.WriteLine("");
+                }
+
+                horse = Console.ReadLine();
+
+                int.TryParse(horse, out  horseInt);
+
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+
+            
+            Console.WriteLine( "You chose horse number " + horses[horseInt] + "!");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("How much do you want to bet?");
+            Console.WriteLine("You have " + money + " dollars");
+
+            string bet = Console.ReadLine();
+
+            int.TryParse(bet, out int betint);
+
+            while (betint > money)
+            {
+                Console.WriteLine("You can't do that sir");
+                Console.WriteLine("Place another bet");
+
+                bet = Console.ReadLine();
+
+                int.TryParse(bet, out betint);
+            }
+
+            money -= betint;
+
+            Console.WriteLine(betint);
+            Console.WriteLine(money);
+
+
+
             //Bestäm användarens pengar
 
             //Kom in till hästbanan
