677360ccea33e6758ecaf83b601483de829afd6a
BandersGit <andy.i.flaskan@gmail.com>
2020-03-25 11:33:31 +0100

    Skapat en while-loop för att låta spelaren spela igen om de inte har slut på pengar.

--- Program.cs
+++ Program.cs
@@ -14,42 +14,19 @@ namespace Slutprojekt
             string[] horses = { "", "1. Roach", "2. Epona", "3. Agro", "4. Rapidash" };
             Random generator = new Random();
             int money = generator.Next(100, 2001);
-            
-            Console.WriteLine("Welcome to the horse races!");
+            bool again = true;
 
-            Break();
-
-            Console.WriteLine("I see you have " + money + " dollars");
-
-            Break();
-
-            Console.WriteLine("Here are todays horses!");
-            Console.WriteLine("");
-
-            for (int i = 1; i < horses.Length; i++)
+            while (again == true && money != 0)
             {
-                Console.WriteLine(horses[i]);
-                Console.WriteLine("");
-            }
-
-            Break();
-
-            Console.WriteLine("Write the number of the horse you want to bet on!");
-
-            string horse = Console.ReadLine();
-
-            int.TryParse(horse, out int horseInt);
-
-            Thread.Sleep(500);
-            Console.Clear();
-
-            while (horseInt == 0 || horseInt > 4)
-            {
-                Console.WriteLine("That is not a valid number, sir");
+                Console.WriteLine("Welcome to the horse races!");
 
                 Break();
 
-                Console.WriteLine("Please choose again");
+                Console.WriteLine("I see you have " + money + " dollars");
+
+                Break();
+
+                Console.WriteLine("Here are todays horses!");
                 Console.WriteLine("");
 
                 for (int i = 1; i < horses.Length; i++)
@@ -58,78 +35,160 @@ namespace Slutprojekt
                     Console.WriteLine("");
                 }
 
-                horse = Console.ReadLine();
+                Break();
 
-                int.TryParse(horse, out horseInt);
+                Console.WriteLine("Write the number of the horse you want to bet on!");
 
-                Console.WriteLine("");
-                Console.WriteLine("");
-            }
+                string horse = Console.ReadLine();
 
-            Thread.Sleep(1000);
-            Console.Clear();
+                int.TryParse(horse, out int horseInt);
 
-            Console.WriteLine("You chose horse number " + horses[horseInt] + "!");
+                Thread.Sleep(500);
+                Console.Clear();
 
-            Break();
+                while (horseInt == 0 || horseInt > 4)
+                {
+                    Console.WriteLine("That is not a valid number, sir");
 
-            Console.WriteLine("How much do you want to bet?");
+                    Break();
 
-            Break();
+                    Console.WriteLine("Please choose again");
+                    Console.WriteLine("");
 
-            Console.WriteLine("You have " + money + " dollars");
+                    for (int i = 1; i < horses.Length; i++)
+                    {
+                        Console.WriteLine(horses[i]);
+                        Console.WriteLine("");
+                    }
 
-            string bet = Console.ReadLine();
+                    horse = Console.ReadLine();
 
-            int.TryParse(bet, out int betInt);
+                    int.TryParse(horse, out horseInt);
 
-            Console.WriteLine("");
-            Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
 
-            while (betInt > money || betInt == 0)
-            {
-                Console.WriteLine("You can't bet that amount sir");
+                Thread.Sleep(1500);
+                Console.Clear();
+
+                Console.WriteLine("You chose horse number " + horses[horseInt] + "!");
 
                 Break();
 
-                Console.WriteLine("Place another bet");
+                Console.WriteLine("How much do you want to bet?");
 
-                bet = Console.ReadLine();
+                Break();
 
-                int.TryParse(bet, out betInt);
+                Console.WriteLine("You have " + money + " dollars");
+
+                string bet = Console.ReadLine();
+
+                int.TryParse(bet, out int betInt);
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                while (betInt > money || betInt == 0)
+                {
+                    Console.WriteLine("You can't bet that amount sir");
+
+                    Break();
+
+                    Console.WriteLine("Place another bet");
+
+                    bet = Console.ReadLine();
+
+                    int.TryParse(bet, out betInt);
+                }
+
+                money -= betInt;
+
+                Break();
+
+                Console.Clear();
+                Console.WriteLine("You bet " + betInt + " dollars on horse number " + horses[horseInt]);
+
+                Break();
+
+                Console.WriteLine("You have " + money + " dollars left");
+
+                Thread.Sleep(2000);
+
+                Console.Clear();
+                Console.WriteLine("It's time to race!");
+
+                int result = generator.Next(1, 5);
+
+                Break();
+
+                Console.WriteLine("And the winner is... " + horses[result] + "!");
+
+                Break();
+
+                money = Winnings(money, betInt, result, horseInt);
+
+                Break();
+                Console.Clear();
+
+                Console.WriteLine("You now have " + money + " dollars!");
+
+                Break();
+
+                Console.WriteLine("Do you want to have another go? (Y/N)");
+
+                string answer = Console.ReadLine().ToUpper();
+
+                Thread.Sleep(1500);
+                Console.Clear();
+
+                while (answer != "Y" && answer != "N")
+                {
+                    Console.WriteLine("Excuse me sir, I couldn't hear you");
+                    Console.WriteLine("Please say Y or N");
+
+                    answer = Console.ReadLine().ToUpper();
+                    Break();
+                }
+
+                if (answer == "Y")
+                {
+                    again = true;
+                    Console.Clear();
+
+                    Console.WriteLine("I'll see you back at the betting site, sir");
+
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    Thread.Sleep(2000);
+                }
+
+                else if (answer == "N")
+                {
+                    again = false;
+                }
             }
 
-            money -= betInt;
-
-            Break();
-
             Console.Clear();
-            Console.WriteLine("You bet " + betInt + " dollars on horse number " + horses[horseInt]);
+            Thread.Sleep(1500);
 
-            Break();
+            if (again == false)
+            {
+                Console.WriteLine("Goodbye sir, your car is waiting for you outside");
+            }
 
-            Console.WriteLine("You have " + money + " dollars left");
+            if (money == 0)
+            {
+                Console.WriteLine("Looks like you are out of money");
 
-            Thread.Sleep(2000);
+                Break();
 
-            Console.Clear();
-            Console.WriteLine("It's time to race!");
+                Console.WriteLine("Unfortunately you will have to leave, sir");
 
-            int result = generator.Next(1, 5);
+                Break();
 
-            Break();
-
-            Console.WriteLine("And the winner is... " + horses[result] + "!");
-
-            Break();
-
-            money = Winnings(money, betInt, result, horseInt);
-
-            Break();
-            Console.Clear();
-
-            Console.WriteLine("You now have " + money + " dollars!");
-
+                Console.WriteLine("Your car will be waiting for you outside");
+            }
             Console.ReadLine();
         }
 
